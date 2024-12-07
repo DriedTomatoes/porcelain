29f02fa1eacd4dae82c0e23985d1680c0b3839bf
27257@student.pwr.edu.pl <272578@student.pwr.edu.pl>
2024-12-07 18:33:55 +0100

    ddd

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,30 @@
+from datetime import datetime
 from django.db import models
 
-# Create your models here.
+
+
+class Employee(models.Model):
+
+    POSITIONS = {
+        "Employee",
+        "Administrator"
+    }
+
+    id = models.IntegerField(primary_key=True)
+    password = models.CharField (max_length=100)
+    login = models.CharField (max_length=100)
+    firstname = models.CharField (max_length=20)
+    lastname = models.CharField(max_length=100)
+    phone = models.CharField (max_length=12)
+    position = models.CharField (max_length=100, choices = POSITIONS)
+
+class Discount(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField (max_length=100)
+    description = models.TextField ()
+    Discount_percentage = models.FloatField (default=0.0)
+    start_date = models.DateField (default=datetime.date)
+    end_date = models.DateField
+
+
+
