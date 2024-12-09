591fb28a50a0bf654339664d13f6a242eeb7830a
27257@student.pwr.edu.pl <272578@student.pwr.edu.pl>
2024-12-09 17:11:09 +0100

    ddd

--- shop/models.py
+++ shop/models.py
@@ -1,14 +1,13 @@
-from datetime import datetime
 from django.db import models
 
 
 
 class Employee(models.Model):
 
-    POSITIONS = {
-        "Employee",
-        "Administrator"
-    }
+    POSITIONS = [
+        ("E","Employee"),
+        ("A","Administrator")
+    ]
 
     id = models.IntegerField(primary_key=True)
     password = models.CharField (max_length=100)
@@ -16,15 +15,19 @@ class Employee(models.Model):
     firstname = models.CharField (max_length=20)
     lastname = models.CharField(max_length=100)
     phone = models.CharField (max_length=12)
-    position = models.CharField (max_length=100, choices = POSITIONS)
+    position = models.CharField (max_length=2, choices = POSITIONS)
+
+class Product(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+
 
 class Discount(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField (max_length=100)
     description = models.TextField ()
     Discount_percentage = models.FloatField (default=0.0)
-    start_date = models.DateField (default=datetime.date)
+    start_date = models.DateField
+    products = models.ManyToManyField(Product)
     end_date = models.DateField
-
-
 
