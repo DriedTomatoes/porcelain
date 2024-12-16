d7b0b5e2d9b1e28ba22cf29ca3139eeba73d6f68
Wiktor Jankowski Ostrowski <122821918+WiktorPWR@users.noreply.github.com>
2024-12-16 11:29:40 +0100

    Delete shop directory

--- shop/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import include, path
-
-from .views import RegisterAPIView, LoginAPIView
-
-urlpatterns = [
-    path("register", RegisterAPIView.as_view()),
-    path("login", LoginAPIView.as_view()),
-
-]
